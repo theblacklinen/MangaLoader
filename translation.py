d494aca59db7d7f6f07c57115fcced92b44a6f5c
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2022-09-24 06:47:11 +0100

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton
 
 class Translation(object):
     START_TEXT = "Hi {},\nI'm MangaLoader!\nYou can upload any manga to Telegram as Zipfile!\n\n/help for more details!"
-    HELP_TEXT = "How to use MangaLoader?!\n\n1. Send manga url from [supported resources](https://github.com/X-Gorn/MangaLoader/tree/cleandir#url-options-and-examples)\n2. Choose subdir option. (will uploaded as Zipfile)\n3. Wait a few minutes and you will receive your archived manga\n\nIf bot didn't respond, contact @xgorn"
+    HELP_TEXT = "How to use MangaLoader?!\n\n1. Send manga url from [supported resources](https://github.com/theblacklinen/MangaLoader/tree/cleandir#url-options-and-examples)\n2. Choose subdir option. (will uploaded as Zipfile)\n3. Wait a few minutes and you will receive your archived manga\n\nIf bot didn't respond, contact @xgorn"
     UPLOAD = 'Choose subdirs option. (will uploaded as Zipfile)'
     CLEANDIR_SUCCESS = 'Successfully cleaned your download dir, now you can sent another link'
     CLEANDIR_UNSUCCESS = 'Your download dir are empty, use this command only if your bot are stuck'
@@ -18,8 +18,8 @@ class Translation(object):
     
     start_buttons=[
         [
-            InlineKeyboardButton('Source', url='https://github.com/X-Gorn/MangaLoader'), 
-            InlineKeyboardButton('Project Channel', url='https://t.me/xTeamBots'),
+            InlineKeyboardButton('Source', url='https://github.com/theblacklinen/MangaLoader'), 
+            InlineKeyboardButton('Project Channel', url='https://t.me/Anime_X_Clan'),
         ],
-        [InlineKeyboardButton('Author', url='https://t.me/xgorn')],
+        [InlineKeyboardButton('Author', url='https://t.me/xoltic')],
     ]
